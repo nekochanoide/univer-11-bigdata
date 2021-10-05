559274e03f8b10c470a3bce03c8019b34da565e4
Matthew Kupriyanov <motya202@gmail.com>
2021-10-05 22:29:50 +0500

    бенчмарк класс без запуска

--- SecondTask.cs
+++ SecondTask.cs
@@ -1,21 +1,60 @@
 ﻿using System.IO;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
 
-int bufferSize = 1024 * 1024;
-int mbs = int.Parse(args[0]);
+Benchy.Mbs = int.Parse(args[0]);
 
-using (FileStream fileStream = new FileStream("./big chungus.txt", FileMode.Create, FileAccess.Write, FileShare.Write))
+// BenchmarkRunner.Run<Benchy>();
+
+[MemoryDiagnoser]
+public class Benchy
 {
-    FileStream fs = new FileStream("./res.txt", FileMode.Open, FileAccess.Read);
-    fileStream.SetLength(fs.Length * mbs);
-    int bytesRead = -1;
-    byte[] bytes = new byte[bufferSize];
+    private const int _bufferSize = 1024 * 1024 * 2;
+    private byte[] _bytes = new byte[_bufferSize];
+    public static int Mbs;
 
-    for (var i = 0; i < mbs; i++)
+    [Benchmark]
+    public void CopyWriteFlow()
     {
-        while ((bytesRead = fs.Read(bytes, 0, bufferSize)) > 0)
+        using (FileStream writeFileStream = new FileStream("./big chungus.txt", FileMode.Create, FileAccess.Write, FileShare.Write))
         {
-            fileStream.Write(bytes, 0, bytesRead);
+            using (FileStream sourceFileStream = new FileStream("./res.txt", FileMode.Open, FileAccess.Read))
+            {
+                writeFileStream.SetLength(sourceFileStream.Length * Mbs);
+                int bytesRead = -1;
+
+                for (var i = 0; i < Mbs; i++)
+                {
+                    while ((bytesRead = sourceFileStream.Read(_bytes, 0, _bufferSize)) > 0)
+                    {
+                        writeFileStream.Write(_bytes, 0, bytesRead);
+                    }
+                    sourceFileStream.Position = 0;
+                }
+            }
         }
-        fs.Position = 0;
+    }
+
+    [Benchmark]
+    public void CopyReadFlow()
+    {
+        using (FileStream writeFileStream = new FileStream("./big chungus.txt", FileMode.Create, FileAccess.Write, FileShare.Write))
+        {
+            using (FileStream sourceFileStream = new FileStream("./res.txt", FileMode.Open, FileAccess.Read))
+            {
+                writeFileStream.SetLength(sourceFileStream.Length * Mbs);
+                int bytesRead = -1;
+                int offset = 0;
+                while ((bytesRead = sourceFileStream.Read(_bytes, 0, _bufferSize)) > 0)
+                {
+                    for (int i = 0; i < Mbs; i++)
+                    {
+                        writeFileStream.Position = i * sourceFileStream.Length + offset;
+                        writeFileStream.Write(_bytes, 0, bytesRead);
+                    }
+                    offset += bytesRead;
+                }
+            }
+        }
     }
 }
