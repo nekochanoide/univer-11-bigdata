809b7ad4103c3a885d8f5511b4139f97c282e72a
Matthew Kupriyanov <motya202@gmail.com>
2021-10-06 00:08:46 +0500

    полный бенчмарк методов

--- SecondTask.cs
+++ SecondTask.cs
@@ -1,24 +1,30 @@
 ﻿using System.IO;
+using System;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
+using System.Collections.Generic;
 
-Benchy.Mbs = int.Parse(args[0]);
-
-// BenchmarkRunner.Run<Benchy>();
+BenchmarkRunner.Run<Benchy>();
 
 [MemoryDiagnoser]
+[RPlotExporter]
+[ShortRunJob]
 public class Benchy
 {
     private const int _bufferSize = 1024 * 1024 * 2;
     private byte[] _bytes = new byte[_bufferSize];
+
+    [ParamsSource(nameof(AllMbsValues))]
     public static int Mbs;
+    public IEnumerable<int> AllMbsValues => new[] { 10, 100, 1_000, 10_000 };
+    private static Random _Random = new Random();
 
     [Benchmark]
     public void CopyWriteFlow()
     {
-        using (FileStream writeFileStream = new FileStream("./big chungus.txt", FileMode.Create, FileAccess.Write, FileShare.Write))
+        using (FileStream writeFileStream = new FileStream($"C:/Users/kuprianov/source/repos/univer-11-bigdata/big chungus/big chungus.txt", FileMode.Create, FileAccess.Write, FileShare.Write))
         {
-            using (FileStream sourceFileStream = new FileStream("./res.txt", FileMode.Open, FileAccess.Read))
+            using (FileStream sourceFileStream = new FileStream("C:/Users/kuprianov/source/repos/univer-11-bigdata/res.txt", FileMode.Open, FileAccess.Read))
             {
                 writeFileStream.SetLength(sourceFileStream.Length * Mbs);
                 int bytesRead = -1;
@@ -38,9 +44,9 @@ public class Benchy
     [Benchmark]
     public void CopyReadFlow()
     {
-        using (FileStream writeFileStream = new FileStream("./big chungus.txt", FileMode.Create, FileAccess.Write, FileShare.Write))
+        using (FileStream writeFileStream = new FileStream($"C:/Users/kuprianov/source/repos/univer-11-bigdata/big chungus/big chungus.txt", FileMode.Create, FileAccess.Write, FileShare.Write))
         {
-            using (FileStream sourceFileStream = new FileStream("./res.txt", FileMode.Open, FileAccess.Read))
+            using (FileStream sourceFileStream = new FileStream("C:/Users/kuprianov/source/repos/univer-11-bigdata/res.txt", FileMode.Open, FileAccess.Read))
             {
                 writeFileStream.SetLength(sourceFileStream.Length * Mbs);
                 int bytesRead = -1;
